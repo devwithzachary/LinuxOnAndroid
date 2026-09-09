f21641abfd3b6954070d6cbfa3cb3b4eb0ee8368
Zachary Powell <zachary@zpweb.site>
2026-09-09 16:43:57 +0100

    Align NDK version to 28.2.13676358 for Gradle and F-Droid reproducibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,6 +9,7 @@ plugins {
 android {
     namespace = "com.devwithzachary.completelinuxinstaller"
     compileSdk = 36
+    ndkVersion = "28.2.13676358"
     defaultConfig {
         applicationId = "com.devwithzachary.completelinuxinstaller"
         minSdk = 23
