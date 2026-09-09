726d1af4ee2980f6fd75cbd1107cfc60f3987aff
Zachary Powell <zachary@zpweb.site>
2026-09-09 16:08:54 +0100

    Remove commented foojay-resolver to pass fdroid scanner

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -25,11 +25,6 @@ dependencyResolutionManagement {
     }
 }
 
-/*
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
-}
-*/
 
 rootProject.name = "LinuxOnAndroid"
 include(":app")
