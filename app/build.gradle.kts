28861065908a70de0a88bb54dbca51c3cd4b2360
Zachary Powell <zachary@zpweb.site>
2026-09-10 11:57:53 +0100

    Filter out incomplete 32-bit x86 ABI from APK packaging

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,6 +16,10 @@ android {
         targetSdk = 36
         versionCode = 13
         versionName = "1.6.0"
+
+        ndk {
+            abiFilters += listOf("arm64-v8a", "armeabi-v7a", "x86_64")
+        }
     }
 
     dependenciesInfo {
